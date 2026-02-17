f097d5c380014e2b2795eb872ccf582a4bafbf79
MedSveta <sveta1978medved@gmail.com>
2026-02-17 20:59:17 +0200

    Type SearchForm

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -76,6 +77,21 @@ public abstract class BasePage {
                 return  (T) new HomePage(driver);
             }
             default -> throw new IllegalArgumentException("Invalid parameter");
+        }
+    }
+
+    public void clickWait(WebElement element, int time){
+        new WebDriverWait(driver, Duration.ofSeconds(time))
+                .until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    public boolean urlContains(String partOfUrl, int time){
+        try {
+            return new WebDriverWait(driver, Duration.ofSeconds(time))
+                    .until(ExpectedConditions.urlContains(partOfUrl));
+        }catch (TimeoutException e){
+            e.printStackTrace();
+            return false;
         }
     }
 }
